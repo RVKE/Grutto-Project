267b6cf207837cb6c14a2cfdacf7e1e2307bfec5
LuukNoverraz <luuk.noverraz@coderclass.nl>
2022-08-05 02:42:06 +0200

    Boat 2077

---
+++ Assets/Scripts/Boat (Luuk)/BoatMouseFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMouseFollow : MonoBehaviour {
+
+	public Camera referenceCamera;
+
+	private BoatMovementController movementController;
+	Vector3 mousePosition = Vector3.zero;
+
+    [Header("Debug Settings")]
+
+	public bool enableDrawMouseGizmo;
+
+	void Start ()
+	{
+		movementController = GetComponent<BoatMovementController>();
+	}
+	
+	void Update ()
+	{
+		Vector3 shipLocation = this.transform.position;
+		Ray cameraRay = referenceCamera.ScreenPointToRay(Input.mousePosition);
+		var rayIterationCount = referenceCamera.transform.position.y / -cameraRay.direction.y;
+		var planeSpaceMouse = new Vector3(cameraRay.origin.x + cameraRay.direction.x * rayIterationCount, 0, cameraRay.origin.z + cameraRay.direction.z * rayIterationCount);
+		mousePosition = planeSpaceMouse;
+		
+		var direction = (mousePosition - shipLocation);
+		if (direction.magnitude > 1)
+		{
+			direction.Normalize();
+		}
+		movementController.horizontal = direction.x;
+		movementController.vertical = direction.z;
+	}
+
+	void OnDrawGizmos()
+	{
+		if (enableDrawMouseGizmo)
+		{
+			Gizmos.color = Color.blue;
+			Gizmos.DrawWireSphere(mousePosition, 0.5f);
+		}
+	}
+}
---
+++ Assets/Scripts/Boat (Luuk)/BoatMovementController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovementController : MonoBehaviour {
+
+    [Header("Boat Settings")]
+
+	public float initialSpeed;
+	public float maxSpeed;
+	public float rotationSpeed;
+
+    [Header("Wave Settings")]
+
+	public float waveHeight;
+	public float waveSpeed;
+	public float waveOffset;
+
+    [Header("Debug Settings")]
+
+	public bool enableVelocityDebug;
+	public bool enableDrawMovementGizmo;
+	
+	public float horizontal;
+	public float vertical;
+
+	private float velocity;
+
+	void Start ()
+	{
+		velocity = initialSpeed;
+	}
+	
+	void Update ()
+	{
+		SetMovement();
+	}
+
+    private void SetMovement()
+    {
+		if (enableVelocityDebug)
+		{
+			Debug.Log("VELOCITY: " + System.Math.Round(velocity, 1));
+		}
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+			if (velocity < maxSpeed)
+			{
+				velocity += 0.025f;
+			}
+		}
+
+		else if (velocity <= initialSpeed)
+		{
+			velocity = initialSpeed;
+		}
+
+		else
+		{
+			velocity -= 0.025f;
+		}
+		
+		// Position
+		var inputDirection = new Vector3(horizontal, 0, vertical);
+		var thrust = Vector3.Dot(inputDirection.normalized, transform.forward);
+		transform.position += thrust * inputDirection.magnitude * transform.forward * velocity * Time.deltaTime;
+		transform.position = new Vector3(transform.position.x, (waveHeight * Mathf.Sin(Time.time * waveSpeed)) + waveOffset, transform.position.z);
+
+		// Rotation
+		var rotation = Vector3.Dot(inputDirection.normalized, transform.right);
+		var rotationAmount = rotationSpeed * Time.deltaTime * rotation;
+		transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + rotationAmount, 0);
+	}
+	
+	void OnDrawGizmos()
+	{
+		var directionVector = new Vector3(horizontal, 0, vertical);
+		var size = Mathf.Min(1, directionVector.magnitude);
+
+		if (enableDrawMovementGizmo)
+		{
+			if (size >= float.Epsilon)
+			{
+				Gizmos.color = Color.grey;
+				Gizmos.DrawWireSphere(transform.position, 4);
+				Gizmos.color = Color.white;
+				Gizmos.DrawWireSphere(transform.position, size * 4);
+			}
+
+			// Draw each component of the input
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(transform.position, transform.position + Vector3.right * horizontal * 4);
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(transform.position, transform.position + Vector3.forward * vertical * 4);
+			// Show the sum of both components
+			Gizmos.color = Color.white;
+			Gizmos.DrawLine(transform.position, transform.position + directionVector * 4);
+		}
+	}
+}
